7a2fb1de9c03390ba9c1f5b7789d983d4197872c
Aditya <adityaoutlier5@gmail.com>
2026-02-05 15:57:06 +0530

    added Task1.py and Task2.py added README

---
+++ TASK1.py
@@ -0,0 +1,12 @@
+a=int(input("Enter a numberA: "))
+b=int(input("Enter a numberB: "))
+sum=a+b
+print("The sum of",a,"and",b,"is",sum)
+diff=a-b
+print("The difference of",a,"and",b,"is",diff)
+prod=a*b
+print("The product of",a,"and",b,"is",prod)
+quotient=a/b
+print("The quotient of",a,"and",b,"is",quotient)
+modulus=a%b
+print("The modulus of",a,"and",b,"is",modulus)
